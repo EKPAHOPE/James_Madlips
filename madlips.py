e58100fc39f89dad0ce1cee2a23b23d0c11193b4
EKPAHOPE <ekpahopejames@gmail.com>
2022-04-26 19:31:06 +0100

    Updated Madlips

--- madlips.py
+++ madlips.py
@@ -4,6 +4,6 @@ verb2 = input("Verb: ")
 famous_person = input("Famous_Person: ")
 
 madlib = f"computer programming is so {adj} it makes me exited all the time because \
-i love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!"
+i love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
 
 print(madlib)
